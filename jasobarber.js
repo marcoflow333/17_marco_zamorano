cefdb0ff2035bdde5adebcd07646dd13bfe603d4
marco.zamorano <marco.zamorano@educa.madrid.org>
2024-11-28 11:46:41 +0100

    sdsd

---
+++ jasobarber.js
@@ -0,0 +1,16 @@
+
+console.log('prueba de código');
+function imprimirMensaje(mensaje) {
+    console.log('prueba de código');
+    alert(mensaje);
+}
+
+var alumnosfila2= ["jacob" , "Alex" ,"David", "Jorge"];
+console.log(alumnosfila2);
+
+var minombre = "Manuel Piña";
+tipodeVariable(minombre);
+
+function tipodeVariable (mivarialbe){
+    let mensaje = "Mi variable es de tipo" + typeof(mivarialbe);
+}
---
+++ jasonbarber09.js
@@ -0,0 +1,10 @@
+var nombre = prompt("Dime tu nombre tete");
+var instituto = prompt("Nombre del instituto");
+document.write("<table border=\"1\"><tr><th>Nombre</th><th>Instituto</th></tr>");
+console.log(nombre);
+document.write("<tr><td>" + nombre + "</td><td>" + instituto + "</td></tr></table>");
+
+var nota1 = 5;
+var nota2 = 4;
+var nototal1= nota1 + nota2;
+var notamedia = (nota1 + nota2) / 2;
